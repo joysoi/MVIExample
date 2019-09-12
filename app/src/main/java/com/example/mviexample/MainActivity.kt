1846cc78a5dabed966f84ac524139c36c3a2521d
Nikola Nikolovski <nikola.joysoi@gmail.com>
2019-09-12 12:34:01 -0400

    initial commit

---
+++ app/src/main/java/com/example/mviexample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mviexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMainFragment()
+    }
+
+    fun showMainFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MainFragment(), "Main Fragment")
+            .commit()
+    }
+}
